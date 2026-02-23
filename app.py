b6b6c1a4b832e0169c9b38ebc9d6ea035353031c
felix <yvonnesitanda@gmail.com>
2026-02-23 23:08:19 +0300

    added files

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def signup():
         password = request.form ["password"]
         phone = request.form ["phone"]
 
+        
         # By use of the print function lets print all those details sent with the upcoming request
        # print(username, email, password , phone)
 
@@ -45,9 +46,49 @@ def signup():
     return jsonify({"message":"User registered successfully"})
 
 
+# Below is the login/sign in route
+@app.route("/api/signin", methods=["POST"])
+def signin():
+    if request.method == "POST" :
+        # Extract the two details on the form
+        email= request.form ["email"]
+        password = request.form ["password"]
+
+        #Print out the details entered
+       # print(email, password)
+
+       # Create/ establish a connection to the database
+       
+    connection = pymysql . connect (host="localhost", user= "root", password="" , database ="sokogardenonline")
+
+    # Create a cursor
+    cursor= connection.cursor(pymysql.cursors.DictCursor)
+
+    #Structure an sql to insert the detail received from the firm
+    sql ="SELECT * FROM users WHERE email = %s AND password =%s"
+
+    # put the data received from the form into a tuple
+    data = (email, password)
+
+    # by use of the cursor execute sql
+    cursor. execute (sql,data)
+
+    #Check whether there are rows returned and store thm on a variable
+    count = cursor. rowcount 
+    # print (count)
 
 
+    # if there are records returned it means the password and the email are correct , other wise it means they are wrong
+    if count== 0 :
+        return jsonify ({"message":"Log in failed"})
+    
+    else:
+        #There must be a user so we create a variable that will hold the detils of the user fetched from the database
+        user = cursor.fetchone()
+        # Return the details to the frontend as well as a message
+        return jsonify ({"message": "User logged in successfully", "user": user})
 
+    
 
 
 
