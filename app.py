433c7c114394b58c35ba6d316a929a3bc2bfa65d
felix <yvonnesitanda@gmail.com>
2026-02-26 11:45:46 +0300

    added files

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # import flask and its components
 from flask import *
+import os
 
 # Import the ptmsql module - it helps us create a connection between python flask and mysql database.
 import pymysql
 
 # Create a flask application and give it a name
 app = Flask(__name__)
+
+# configure the location to where your product images will be saved on your application
+app.config["UPLOAD_FOLDER"]= "static/images"
 
 
 # Below is the sign up route
@@ -87,10 +91,129 @@ def signin():
         user = cursor.fetchone()
         # Return the details to the frontend as well as a message
         return jsonify ({"message": "User logged in successfully", "user": user})
+    
+    #Below is the route for adding product
+@app.route("/api/add_product",methods=["POST"])
+def Addproducts():
+    if request.method == "POST":
+        # Extract the data entered on the form
+        product_name= request.form ["product_name"]
+        product_description= request.form ["product_description"]
+        product_cost= request.form["product_cost"]
+        #For the product photo, we shall fetch it from the files as shown below
+        product_photo= request.files["product_photo"]
+
+        # Extract the filename of the product photo
+        filename= product_photo.filename
+        # by use of the os module (operating system) we can extract the file path where the images is currently saved.
+        photo_path=os.path.join(app.config["UPLOAD_FOLDER"],filename) 
+        #save the product photo image into the new location
+        product_photo.save(photo_path)
+
+        # Print them out to test whether you are receiving the details sent with the request.
+        #print(product_name, product_description, product_cost, product_photo)
+
+        # establish  connection to the database
+        connection = pymysql.connect(host="localhost", user="root",password="",database="sokogardenonline")
+
+        # create a cursor
+        cursor= connection.cursor()
+        sql= "INSERT INTO product_details(product_name,product_description,product_cost,product_photo) VALUES(%s, %s, %s,%s)"
+
+      # create a tuple tht will hold the data from the which are current held onto different variable declared.
+        data=(product_name, product_description,product_cost,filename)
+
+     # By use of the cursor , execute the sql as you replace the placeholders with actual valu
+    cursor.execute(sql,data)
+
+    # Commit the changes to the database
+    connection.commit()
+
+    return jsonify ({"message" :"Product added successfully"})
+
+# Below is the route of getting products
+@app.route("/api/get_products")
+def get_products():
+
+    # Create a connection to database
+    connection = pymysql.connect (host = "localhost", user="root", password ="", database = "sokogardenonline")
+
+    # Create a cursor
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+    #structure the query to fetch all data from the table
+    sql= "SELECT * FROM product_details"
+
+    # Execute the query
+    cursor.execute(sql)
+
+    # Create a variable that will hold the data fetched from the table
+    products= cursor.fetchall()
+
+    return jsonify(products)
+
+
+    # Mpesa Payment Route/Endpoint 
+import requests
+import datetime
+import base64
+from requests.auth import HTTPBasicAuth
+ 
+@app.route('/api/mpesa_payment', methods=['POST'])
+def mpesa_payment():
+    if request.method == 'POST':
+        amount = request.form['amount']
+        phone = request.form['phone']
+        # GENERATING THE ACCESS TOKEN
+        # create an account on safaricom daraja
+        consumer_key = "GTWADFxIpUfDoNikNGqq1C3023evM6UH"
+        consumer_secret = "amFbAoUByPV2rM5A"
+ 
+        api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"  # AUTH URL
+        r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+ 
+        data = r.json()
+        access_token = "Bearer" + ' ' + data['access_token']
+ 
+        #  GETTING THE PASSWORD
+        timestamp = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
+        passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+        business_short_code = "174379"
+        data = business_short_code + passkey + timestamp
+        encoded = base64.b64encode(data.encode())
+        password = encoded.decode('utf-8')
+ 
+        # BODY OR PAYLOAD
+        payload = {
+            "BusinessShortCode": "174379",
+            "Password": "{}".format(password),
+            "Timestamp": "{}".format(timestamp),
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": "1",  # use 1 when testing
+            "PartyA": phone,  # change to your number
+            "PartyB": "174379",
+            "PhoneNumber": phone,
+            "CallBackURL": "https://modcom.co.ke/api/confirmation.php",
+            "AccountReference": "account",
+            "TransactionDesc": "account"
+        }
+ 
+        # POPULAING THE HTTP HEADER
+        headers = {
+            "Authorization": access_token,
+            "Content-Type": "application/json"
+        }
+ 
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"  # C2B URL
+ 
+        response = requests.post(url, json=payload, headers=headers)
+        print(response.text)
+        return jsonify({"message": "Please Complete Payment in Your Phone and we will deliver in minutes"})
+
 
     
 
 
 
-# Run the application
-app.run(debug= True)
+# run the application
+app.run(debug=True)
---
+++ sokogardenonline.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 25, 2026 at 07:36 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sokogardenonline`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_details`
+--
+
+CREATE TABLE `product_details` (
+  `product_id` int(50) NOT NULL,
+  `product_name` varchar(200) NOT NULL,
+  `product_description` varchar(10000) NOT NULL,
+  `product_cost` int(50) NOT NULL,
+  `product_photo` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_details`
+--
+
+INSERT INTO `product_details` (`product_id`, `product_name`, `product_description`, `product_cost`, `product_photo`) VALUES
+(1, 'test', 'test', 40000, 'smartphone.jpg'),
+(3, 'Android Smartphone', 'This is a very good phone.', 25400, 'smartphone.jpg'),
+(4, 'Android Smartphone', 'This is a very good phone.', 25400, 'smartphone.jpg'),
+(5, 'Skin care', 'This is a very good skin care for dry skin.', 13000, 'prod2.jpg'),
+(6, 'Laptop', 'This is a very good laptop with a big ram.', 35000, 'prod3.jpg'),
+(7, 'Ipad', 'This is a very good ipad.', 25000, 'prod4.avif'),
+(8, 'Ipad', 'This is a very good ipad.', 25000, 'prod4.avif'),
+(9, 'Ipad', 'This is a very good ipad.', 25000, 'prod4.avif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(50) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `phone`, `password`) VALUES
+(1, 'Yvonne', 'ynekesa@gmail.com', '254722976247', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product_details`
+--
+ALTER TABLE `product_details`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`,`email`,`phone`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product_details`
+--
+ALTER TABLE `product_details`
+  MODIFY `product_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
